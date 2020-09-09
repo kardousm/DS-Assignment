03dd37e6be65720106247f3a0991d8b89a397dfe
kardousm <35500433+kardousm@users.noreply.github.com>
2020-09-09 14:46:28 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import re
 
-
 def create_list():
     temp = 1
     intervals = []
-    f = open('1.in', 'r')
+    f = open('10.in', 'r')
     f1 = f.readlines()
     for line in f1:
         if temp == 1:
@@ -14,6 +13,7 @@ def create_list():
             x = re.sub('\s+', ",", line.strip())
             int_interval = convert_str_to_int(x)
             intervals.append(int_interval)
+    intervals.sort(key=lambda x:x[0])
     return intervals
 
 def convert_str_to_int(s):
@@ -35,13 +35,20 @@ def combine(time_intervals):
             y += 1
     return temporary_list
 
-def compute(final_intervals):
+def compute2(intervals):
     val = 0
-    for item in final_intervals:
+    for item in intervals:
         val += item[1] - item[0]
     return val
 
 
+def compute1(final_intervals):
+    if isinstance(final_intervals, list):
+        return final_intervals[1] - final_intervals[0]
+    else:
+        return final_intervals
+
+#First Iteration
 def run():
     untouched_list = create_list()
     max_coverage = 0
@@ -51,7 +58,7 @@ def run():
         touched_list = [x[:] for x in untouched_list]
         touched_list.pop(i)
         new_interval = combine(touched_list)
-        coverage = compute(new_interval)
+        coverage = compute1(new_interval)
         print('{}% Completed'.format((i / len(untouched_list)) * 100))
         if coverage > max_coverage:
             max_coverage = coverage
@@ -60,11 +67,40 @@ def run():
 
     #max_time_interval gives you the time interval of the life guard who has minimum impact on pool coverage
 
-    f = open('1.out', 'w+')
-    f.write(str(max_coverage))
+    #f = open('1.out', 'w+')
+    #f.write(str(max_coverage))
 
     return max_coverage
 
 
+#Second Iteration
+def least_affected(temp_list):
+    print(temp_list)
+    temp = [x[:] for x in temp_list]
+    for i in range(0, len(temp_list) - 1):
+        if temp[i][1] > temp[i + 1][0]:
+            overlap = temp[i][1] - temp[i + 1][0]
+            temp_list[i] = compute1(temp_list[i]) - overlap
+            temp_list[i + 1] = compute1(temp_list[i + 1]) - overlap
+
+        else:
+            temp_list[i] = compute1(temp_list[i])
+            temp_list[i + 1] = compute1(temp_list[i + 1])
+
+    return temp_list
+
+
+def run_2():
+    list1 = create_list()
+    temp_list = [x[:] for x in list1]
+    maximum_time = compute2(combine(list1))
+    least_affected_list = least_affected(temp_list)
+    result = maximum_time - min(least_affected_list)
+    return result
+
+
 if __name__ == "__main__":
-    run()
+    print(run_2())
+
+
+
